c788fc4282b264b3e543c68af02145adaeef104c
Siwan19 <68042209+Siwan19@users.noreply.github.com>
2020-07-09 14:15:23 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
-music.playMelody("E G B A F D A B ", 120)
+basic.showString("BOoFRere")
+basic.pause(500)
+basic.showNumber(10256)
+basic.pause(500)
+basic.showArrow(ArrowNames.NorthEast)
+basic.pause(500)
+basic.showIcon(IconNames.Happy)
 basic.forever(function () {
 	
 })
